f25dbb89382da92b3c47adb38aeee35654b722e2
chancemusselman <musselman.chance@icloud.com>
2018-06-22 16:01:03 -0700

    Finished basic Ebay Function/formating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 $("document").ready(function () {
     console.log('Ready');
+    let ebaySearchWord = "spiderman comics"
 
-    let queryUrl = "http://open.api.ebay.com/shopping?version=515&callname=FindItems&appid=ChanceMu-ClassPro-PRD-667ac8c8b-ab199383&QueryKeywords=spiderman&ItemSort=PricePlusShipping&responseencoding=JSON";
+    let queryUrl = "http://open.api.ebay.com/shopping?version=515&callname=FindItems&appid=ChanceMu-ClassPro-PRD-667ac8c8b-ab199383&QueryKeywords="+ebaySearchWord+"&ItemSort=PricePlusShipping&responseencoding=JSON&MaxEntries=10";
 
     $.ajax({
         url: queryUrl,
@@ -18,25 +19,38 @@ $("document").ready(function () {
 
 function DisplayEbayResults(test){
     for(let i = 0; i < test.Item.length; i++) {
+        let div = $("<div>");
+        div.addClass("row");
+        div.css("margin", "20px");
         let img = $("<img>");
         img.attr("src", test.Item[i].GalleryURL);
         img.css("width", "200px");
         img.css("height", "200px");
-        $('#ebayResults').append(img);
+        img.css("float", "left");
+        img.css("margin-right", "10px");
+        // $('#ebayResults').append(img);
         let title = $('<h4>');
         title.html(test.Item[i].Title)
-        $('#ebayResults').append(title);
+        // $('#ebayResults').append(title);
         let link = $("<a>BUY</a>");
         link.attr("href", test.Item[i].ViewItemURLForNaturalSearch);
-        $('#ebayResults').append(link);
+        link.addClass("btn");
+        link.addClass("btn-success");
+        // $('#ebayResults').append(link);
         let bids = $('<div>');
-        bids.html("Total Bids: "+test.Item[i].BidCount);
-        $('#ebayResults').append(bids);
+        bids.html("Total Bids: "+test.Item[i].BidCount+"<br><br>");
+        // $('#ebayResults').append(bids);
         let price = $("<h2>");
-        price.html(test.Item[i].ConvertedCurrentPrice.Value);
-        $('#ebayResults').append(price);
+        price.text("$"+test.Item[i].ConvertedCurrentPrice.Value);
+        let ebayLogo = $("<img>");
+        ebayLogo.attr("src", "assets/images/ebayLogo.png");
+        ebayLogo.css("height", "25px");
+        ebayLogo.css("float", "right");
+        div.append(img, title, bids, price, ebayLogo,link );
+        // $('#ebayResults').append(price);
+        $('#ebayResults').append(div);
     };
-}
+};
 
 
 
