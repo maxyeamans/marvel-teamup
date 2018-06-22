278e6f7acd67762c984b4a91d8c7f9764756568d
Legend <jasonrfelipe@gmail.com>
2018-06-22 15:19:45 -0700

    added second character search and added a search of character1 and character2 comics. HTML spits out 3 images of the comics they have been in

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,64 +22,131 @@ var database = firebase.database();
 
 
 var strCharacterName;
-var numCharacterId;
-var imgCharacterThumb;
-var imgCharacterMain;
+var strCharacterName2;
 
-var strCharacterFolder;
+var numCharacterId;
+var numCharacterId2;
+
+var imgCharacterThumb;
+var imgCharacterThumb2;
+
 var strNameFolder;
 
-var charNumber;
+var strCollabTitle1;
+var numCollabId1;
+var imgCollabThumb1;
+
+var strCollabTitle2;
+var numCollabId2;
+var imgCollabThumb2;
+
+
+var strCollabTitle3;
+var numCollabId3;
+var imgCollabThumb3;
+
+
+
+//----First Character----
 $("#characterInput").on("click", function (event) {
     event.preventDefault();
 
     strCharacterName = $("#userInput").val();
-
     var queryURL = "http://gateway.marvel.com/v1/public/characters?name=" + strCharacterName + "&ts=1&apikey=4287eee52c27f292e44137f86910da4a&hash=3f4394a993af3110f684ed8d0f8db35d"
-    
+
     $.ajax({
         url: queryURL,
-        method: "GET"
-    }).then(function (response) {
+        method: "GET",
+    }).then(function (firstCharacter) {
 
-        imgCharacterThumb = response.data.results[0].thumbnail.path + "." + response.data.results[0].thumbnail.extension;
-        numCharacterId = response.data.results[0].id;
-        strCharacterName = response.data.results[0].name;
+        imgCharacterThumb = firstCharacter.data.results[0].thumbnail.path + "." + firstCharacter.data.results[0].thumbnail.extension;
+        numCharacterId = firstCharacter.data.results[0].id;
+        strCharacterName = firstCharacter.data.results[0].name;
+
+        if (typeof firstCharacter == "undefined") {
+            $('#testImage').html("CANNOT FIND CHARACTER!")
+
+        }
+        else {
+
+            database.ref(numCharacterId + "/").set({
+                idNumber: numCharacterId,
+                name: strCharacterName,
+                thumbnail: imgCharacterThumb,
+
+            });
+            $('#testImage').html("<img style='width:300px; height:300px' src=" + imgCharacterThumb + "></img>")
+            $('#firstCharacterName').text(strCharacterName)
+
+        }
+    });
+});
+
+//----Second Character----
+$("#characterInput2").on("click", function (event) {
+    event.preventDefault();
+
+    strCharacterName2 = $("#userInput2").val();
+    var queryURL2 = "http://gateway.marvel.com/v1/public/characters?name=" + strCharacterName2 + "&ts=1&apikey=4287eee52c27f292e44137f86910da4a&hash=3f4394a993af3110f684ed8d0f8db35d"
+
+    $.ajax({
+        url: queryURL2,
+        method: "GET",
+    }).then(function (secondCharacter) {
 
 
-        charNumber = response.data.results[0].id;
-        console.log(response.data.results[0].id);
-        database.ref(charNumber+"/").set({
-            idNumber : numCharacterId,
-            name: strCharacterName,
-            thumbnail: imgCharacterThumb,
+        imgCharacterThumb2 = secondCharacter.data.results[0].thumbnail.path + "." + secondCharacter.data.results[0].thumbnail.extension;
+        numCharacterId2 = secondCharacter.data.results[0].id;
+        strCharacterName2 = secondCharacter.data.results[0].name;
 
-        })
+        if (typeof secondCharacter == "undefined") {
+            $('#secondTestImage').html("CANNOT FIND CHARACTER!")
 
-    })
-    // console.log(queryURL);
+        }
+        else {
 
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function (response) {
+            database.ref(numCharacterId + "/").set({
+                idNumber: numCharacterId2,
+                name: strCharacterName2,
+                thumbnail: imgCharacterThumb2,
 
-    //     imgCharacterThumb = response.data.results[0].thumbnail.path + "." + response.data.results[0].thumbnail.extension;
-    //     numCharacterId = response.data.results[0].id;
-    //     strNameFolder = response.data.results[0].name;
+            });
+            $('#secondTestImage').html("<img style='width:300px; height:300px' src=" + imgCharacterThumb2 + "></img>")
+            $('#secondCharacterName').text(strCharacterName2)
+        }
+    });
+});
 
-    //     //database.ref(strCharacterFolder).on("value", function (snapshot){
-    //         //if (snapshot.child().exists === true) {
-    //             //database.ref(strCharacterFolder).push({
-    //             database.ref(strNameFolder).push({
-    //                 name: strCharacterName,
-    //                 id: numCharacterId,
-    //                 thumbnail: imgCharacterThumb,
-    //             });
-    //     //}
-    //     //});
-        $('#testImage').html("<img style='width:300px; height:300px' src=" + imgCharacterThumb + "></img>")
+
+//----Collab----
+$('#fusion').on("click", function () {
+    var queryURL3 = "https://gateway.marvel.com:443/v1/public/comics?format=comic&noVariants=false&dateRange=1960-01-01%2C%202018-6-22&characters=" + numCharacterId + "&sharedAppearances=" + numCharacterId2 + "&ts=1&apikey=4287eee52c27f292e44137f86910da4a&hash=3f4394a993af3110f684ed8d0f8db35d"
+    console.log(queryURL3)
+
+    $.ajax({
+        url: queryURL3,
+        method: "GET",
+    }).then(function (theCollab) {
+        console.log(theCollab)
+
+        strCollabTitle1 = theCollab.data.results[0].title;
+        numCollabId1 = theCollab.data.results[0].id;
+        imgCollabThumb1 = theCollab.data.results[0].thumbnail.path + "." + theCollab.data.results[0].thumbnail.extension
+
+        strCollabTitle2 = theCollab.data.results[1].title;
+        numCollabId2 = theCollab.data.results[1].id;
+        imgCollabThumb2= theCollab.data.results[1].thumbnail.path + "." + theCollab.data.results[1].thumbnail.extension
+
+        strCollabTitle3= theCollab.data.results[2].title;
+        numCollabId3 = theCollab.data.results[2].id;
+        imgCollabThumb3 = theCollab.data.results[2].thumbnail.path + "." + theCollab.data.results[2].thumbnail.extension
+
+
+
+        $('#comic1').html("<img style='width:300px; height:300px' src=" + imgCollabThumb1 + "></img>")
+        $('#comic2').html("<img style='width:300px; height:300px' src=" + imgCollabThumb2 + "></img>")
+        $('#comic3').html("<img style='width:300px; height:300px' src=" + imgCollabThumb3 + "></img>")
+
 
     });
-
-// });
+});
