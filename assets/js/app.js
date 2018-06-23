a90c080cee9c6e622ed304d4de6d230d93a59882
Legend <jasonrfelipe@gmail.com>
2018-06-22 18:17:16 -0700

    updated js and made a for loop to check character names in ajax data

--- assets/js/app.js
+++ assets/js/app.js
@@ -119,34 +119,60 @@ $("#characterInput2").on("click", function (event) {
 
 
 //----Collab----
+
+
+//Stuff to fix:
+//Shows first character, but if second character is not found then it runs and shows only first character comics.
+
+//Need to input a if/then searching theCollab.data.results.characters
+
 $('#fusion').on("click", function () {
-    var queryURL3 = "https://gateway.marvel.com:443/v1/public/comics?format=comic&noVariants=false&dateRange=1960-01-01%2C%202018-6-22&characters=" + numCharacterId + "&sharedAppearances=" + numCharacterId2 + "&ts=1&apikey=4287eee52c27f292e44137f86910da4a&hash=3f4394a993af3110f684ed8d0f8db35d"
+    var queryURL3 = "https://gateway.marvel.com:443/v1/public/comics?format=comic&noVariants=false&dateRange=1960-01-01%2C%202018-6-22&characters=" + numCharacterId + "&sharedAppearances=" + numCharacterId2 + "&orderBy=onsaleDate&ts=1&apikey=4287eee52c27f292e44137f86910da4a&hash=3f4394a993af3110f684ed8d0f8db35d"
     console.log(queryURL3)
 
     $.ajax({
         url: queryURL3,
         method: "GET",
     }).then(function (theCollab) {
-        console.log(theCollab)
 
-        strCollabTitle1 = theCollab.data.results[0].title;
-        numCollabId1 = theCollab.data.results[0].id;
-        imgCollabThumb1 = theCollab.data.results[0].thumbnail.path + "." + theCollab.data.results[0].thumbnail.extension
+        //just to shorten the code
+        var results = theCollab.data.results
 
-        strCollabTitle2 = theCollab.data.results[1].title;
-        numCollabId2 = theCollab.data.results[1].id;
-        imgCollabThumb2= theCollab.data.results[1].thumbnail.path + "." + theCollab.data.results[1].thumbnail.extension
+        //this loops through the results array
+        for (i = 0; i < results.length; i++) {
+            //this loops through the items array which is inside the results array
+            for (k = 0; k < results[i].characters.items.length; k++) {
+                var strNameLoop = results[i].characters.items[i].name
 
-        strCollabTitle3= theCollab.data.results[2].title;
-        numCollabId3 = theCollab.data.results[2].id;
-        imgCollabThumb3 = theCollab.data.results[2].thumbnail.path + "." + theCollab.data.results[2].thumbnail.extension
-
+                // console.log(strNameLoop) // this spits out character names that are inside the comics. 
+                console.log(strNameLoop == strCharacterName2); //console logs the loop and sees if it's true.
 
 
-        $('#comic1').html("<img style='width:300px; height:300px' src=" + imgCollabThumb1 + "></img>")
-        $('#comic2').html("<img style='width:300px; height:300px' src=" + imgCollabThumb2 + "></img>")
-        $('#comic3').html("<img style='width:300px; height:300px' src=" + imgCollabThumb3 + "></img>")
+                //needs code that checks if the second character name shows up. 
 
 
+                //if second character's name does not show up, must show error message saying it does not. 
+
+
+                //these will be changed to the comics that show up with matching characters.
+                strCollabTitle1 = theCollab.data.results[0].title;
+                numCollabId1 = theCollab.data.results[0].id;
+                imgCollabThumb1 = theCollab.data.results[0].thumbnail.path + "." + theCollab.data.results[0].thumbnail.extension
+
+                strCollabTitle2 = theCollab.data.results[1].title;
+                numCollabId2 = theCollab.data.results[1].id;
+                imgCollabThumb2 = theCollab.data.results[1].thumbnail.path + "." + theCollab.data.results[1].thumbnail.extension
+
+                strCollabTitle3 = theCollab.data.results[2].title;
+                numCollabId3 = theCollab.data.results[2].id;
+                imgCollabThumb3 = theCollab.data.results[2].thumbnail.path + "." + theCollab.data.results[2].thumbnail.extension
+
+
+
+                $('#comic1').html("<img style='width:300px; height:300px' src=" + imgCollabThumb1 + "></img>")
+                $('#comic2').html("<img style='width:300px; height:300px' src=" + imgCollabThumb2 + "></img>")
+                $('#comic3').html("<img style='width:300px; height:300px' src=" + imgCollabThumb3 + "></img>")
+            }
+        }
     });
 });
